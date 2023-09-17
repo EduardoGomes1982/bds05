a60e45d4b0f7441576ec96fb0553b76e52c8a889
EduardoGomes1982 <edu1982talk@gmail.com>
2023-09-17 11:33:00 -0300

    Fix imports

--- src/main/java/com/devsuperior/movieflix/entities/Review.java
+++ src/main/java/com/devsuperior/movieflix/entities/Review.java
@@ -1,8 +1,5 @@
 package com.devsuperior.movieflix.entities;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
